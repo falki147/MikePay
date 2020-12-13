ec0b8acbb8311be4f088917b8bf56eb7353d42ae
Florian Preinfalk <preinfalkflorian@gmail.com>
2020-12-13 16:29:13 +0100

    Copy libs to dist and add clean task

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'libs', to: 'libs' }
+      ]
+    })
+  ],
   entry: [
     './src/js/index.js',
     './src/css/index.css'
