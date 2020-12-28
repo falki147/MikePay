7bce0932ec937722501d86d6e4189bc870e543b5
Bennyburg <benjamin.burglechner@gmail.com>
2020-12-28 10:19:39 +0100

    updates

---
+++ src/js/forms/create-order.js
@@ -0,0 +1,41 @@
+import Api from "../api/api";
+
+window.addEventListener("load", function () {
+  document.getElementById("createOrder-form").addEventListener("submit", validateCreateOrderForm);
+});
+
+function validateCreateOrderForm(e){
+
+  const form = document.getElementById("createOrder-form");
+  let titel = document.getElementById("titel").value;
+  let url = document.getElementById("url").value;
+  let description = document.getElementById("description").value;
+  
+  let flag = 1;
+
+  let reg_titel = /\w+/;
+  let reg_url = /\w+/;
+  let reg_desc = /\w*/;
+
+  if(!reg_titel.test(titel)){
+    document.getElementById("titel-val").innerText = "Bitte geben Sie einen Titel an!";
+    flag = 0;
+  }
+
+  if(!reg_url.test(url)){
+    document.getElementById("url-val").innerText = "Bitte geben Sie einen URL an!";
+    flag = 0;
+  }
+
+  if(!reg_desc.test(description)){
+    document.getElementById("description-val").innerText = "Bitte geben Sie eine Beschreibung an!";
+    flag = 0;
+  }
+
+  if(!flag || !form.checkValidity()){
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  form.classList.add('was-validated');
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import "bootstrap";
 import "./forms/login-form";
+import "./forms/create-user";
+import "./forms/create-order";
+import "./tables/load_enter_debts";
 import "./tables/debtors";
