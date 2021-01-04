37a31607ea2b418192d2498e082e45038e040c1a
Florian Preinfalk <preinfalkflorian@gmail.com>
2021-01-04 19:34:37 +0100

    Refactor login form

--- src/js/forms/login-form.js
+++ src/js/forms/login-form.js
@@ -2,24 +2,27 @@ import Api from "../api/api";
 import Alert from "../components/alert";
 import Loader from "../components/loader";
 
-const loginForm = document.getElementById("login-form");
-if (loginForm) {
-  loginForm.addEventListener("submit", async ev => {
-    ev.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
+  if (loginForm) {
+    loginForm.addEventListener("submit", async ev => {
+      ev.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
 
-    try {
-      Loader.begin(document.getElementById("login-btn"));
-      await Api.login(username, password);
-      Alert.success("Login war erfolgreich!");
-      window.location = "/";
-    }
-    catch (e) {
-      Alert.error(e.message);
-    }
+      try {
+        Loader.begin(document.getElementById("login-btn"));
+        await Api.login(username, password);
+        Alert.success("Login war erfolgreich.");
+        window.location = "/";
+      }
+      catch (e) {
+        console.error(e);
+        Alert.error(e.message);
+      }
 
-    Loader.end();
-  });
-}
+      Loader.end();
+    });
+  }
+});
