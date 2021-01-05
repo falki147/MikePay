db889462d9985a9e1cbefe125dd93bfd709ec158
Florian Preinfalk <preinfalkflorian@gmail.com>
2021-01-05 15:36:14 +0100

    Refactor card links

--- src/js/components/startpage.js
+++ src/js/components/startpage.js
@@ -5,9 +5,7 @@ import Session from "../api/session";
  */
 document.addEventListener("DOMContentLoaded", async () => {
   const debtLink = document.getElementById("startpage-debts-link");
-  const debtLinkCard = document.getElementById("startpage-debts-link-card");
-  if (debtLink && debtLinkCard && await Session.isLoggedIn()) {
+  if (debtLink && await Session.isLoggedIn()) {
     debtLink.href = `/debtor/?user_id=${await Session.id()}`;
-    debtLinkCard.href = `/debtor/?user_id=${await Session.id()}`; 
   }
 });
