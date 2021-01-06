79b032f5eb383ec1c26e58c99445278ef7cb651d
Florian Preinfalk <preinfalkflorian@gmail.com>
2021-01-06 09:53:39 +0100

    Fix short date on safari

--- src/js/utils/dateShort.js
+++ src/js/utils/dateShort.js
@@ -1,8 +1,20 @@
 /**
  * Formats a date in the DD.MM.YYYY format
- * @param {String} fullDate
+ * @param {String} fullDate in "YYYY-MM-DD hh:mm" format
  */
-export default function getShortDate(fullDate){
-  const date = new Date(fullDate);
-  return date.getDate() + "." + (date.getMonth()+1) + "." + date.getFullYear();
+export default function getShortDate(fullDate) {
+  const match = fullDate.match(/^(\d{4})-(\d{1,2})-(\d{1,2})/);
+
+  if (!match) {
+    return NaN;
+  }
+
+  const year = Number(match[1]);
+  const month = Number(match[2]);
+  const day = Number(match[3]);
+
+  const paddedDay = (day < 10 ? "0" : "") + day;
+  const paddedMonth = (month < 10 ? "0" : "") + month;
+
+  return `${paddedDay}.${paddedMonth}.${year}`;
 };
